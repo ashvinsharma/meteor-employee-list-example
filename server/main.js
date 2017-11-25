483f76796fe838951ef030c546280dd088f881d0
ashvinsharma <ashvinsharma97@gmail.com>
2017-11-25 19:48:38 +0530

    first and last push

---
+++ server/main.js
@@ -0,0 +1,22 @@
+import _ from 'lodash'
+import {Meteor} from 'meteor/meteor'
+import {Employees} from '../imports/collections/employees'
+import {image, helpers} from 'faker'
+
+Meteor.startup(() => {
+    const numberRecords = Employees.find({}).count()
+    if (!numberRecords) {
+        _.times(5000, () => {
+            const {name, email, phone} = helpers.createCard()
+
+            Employees.insert({
+                name, phone, email,
+                avatar: image.avatar()
+            })
+        })
+    }
+
+    Meteor.publish('employees', per_page => {
+        return Employees.find({}, {limit: per_page})
+    })
+})
